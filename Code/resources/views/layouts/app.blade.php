348cdfdea28d665788338bad953099dd5d8dbef1
Basil <you@example.com>
2022-07-23 01:10:08 +0300

    main

--- Code/resources/views/layouts/app.blade.php
+++ Code/resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="{{ asset('js/app.js') }}" defer></script>
         @livewireStyles
 
         <!-- Scripts -->
--- Code/resources/views/layouts/guest.blade.php
+++ Code/resources/views/layouts/guest.blade.php
@@ -11,6 +11,8 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="{{ asset('js/app.js') }}" defer></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
